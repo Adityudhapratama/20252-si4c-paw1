e862499eb5aff47fc4b1837ee6fcad5364bb2dc2
Adityudhapratama <pratamaadityudha@gmail.com>
2026-04-06 13:12:03 +0700

    local storage

---
+++ localstorage/script.js
@@ -0,0 +1,9 @@
+//simpan ke local storage
+localStorage.setItem("npm", 123);
+localStorage.setItem("nama", "yudha");
+
+//baca data local storage
+console.log(localStorage.getItem("npm"))
+
+//tampilkan di paragraf id=npm 
+document.getElementById("npm").innerHTML = localStorage.getItem("npm")
---
+++ localstorage/scriptls.js
@@ -0,0 +1,54 @@
+let npm = document.getElementById("npm");
+let nama = document.getElementById("nama");
+
+function simpan() {
+    console.log(npm.value)
+    console.log(nama.value)
+
+    // localStorage.setItem("npm", npm.value)
+    l//ocalStorage.setItem("nama", nama.value)
+
+    //cek local storage sudah ada isi atau belum
+
+    //jika local storage belum ada isi/value
+    if (localStorage.getItem("mahasiswa") === null) {
+        //simpan array kosong 
+        localStorage.setItem("mahasiswa", "[]")
+    }
+
+    //panggil local storage 
+    let data = JSON.parse(localStorage.getItem("mahasiswa"))
+    console.log(data)
+
+
+    //simpan value npm da nama ke dalam object
+    data.push({
+        nama: npm.value,
+        nama: nama.value
+    })
+    console.log(data)
+
+    //simpan data terbaru ke dalam local storage
+    //konversi dari object menjadi string
+
+    localStorage.setItem("mahasiswa", JSON.stringify(data))
+
+    //tampil 
+    tampil()
+
+    //clear
+    document.getElementById("list-mhs").innerHTML=""
+
+
+
+    function tampil() {
+        //panggil dulu local storage
+        let hasil = JSON.parse(localStorage.getItem("mahasiswa"))
+
+        //lakukan perulangan (forEach)
+        hasil.forEach(element => {
+            //console.log(element)
+            document.getElementById("list-mhs").innerHTML += <li>${element.npm} ${element.nama}</li>
+        });
+    }
+}
